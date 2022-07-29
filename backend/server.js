fb4aa7f290143ee211b4c5f3e0f2fbf35e681161
Radek Sobieralski <radek4104@gmail.com>
2022-07-29 12:20:36 +0200

    add router

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const workoutRoutes = require('./routes/workouts')
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.get("/", (req, res) => {
-    res.json({message: 'Welcome to the app'})
-})
+app.use('/api/workouts/', workoutRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port', process.env.PORT)
